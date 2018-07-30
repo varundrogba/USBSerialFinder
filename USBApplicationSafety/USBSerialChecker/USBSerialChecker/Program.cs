163b3857b3e5fb8c9ef0508d2f1611c7bb3ce9a3
Varun Shah <cvp2067@varian.com>
2018-07-30 11:56:28 -0700

    Changed project directory hierarchy

---
+++ USBApplicationSafety/USBSerialChecker/USBSerialChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Management;
+using System.IO;
+
+namespace USBSerialFinder
+{
+    class USBSerialTextFile
+    {
+        static void Main(string[] args)
+        {
+            //Path where the txt file will be created. This is the path of the folder where the exe will be stored.
+            string path = AppDomain.CurrentDomain.BaseDirectory + "FileName.txt";
+            ManagementObjectSearcher Search = new ManagementObjectSearcher(Constants.Query);
+            foreach (ManagementObject currentObject in Search.Get())
+            {
+                ManagementObject Query = new ManagementObject(Constants.WindowsPhysicalMediaTag + "='" + currentObject[Constants.DeviceID] + "'");
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                    TextWriter tw = new StreamWriter(path);
+                    tw.WriteLine(Query[Constants.SerialNumber].ToString());
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
