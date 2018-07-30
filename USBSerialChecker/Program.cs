c31dc134bdaea69dadc8e335b66e43818264417a
Varun Shah <varundrogba@gmail.com>
2018-07-30 11:37:59 -0700

    Fixed missing FileName Bug

--- USBSerialChecker/Program.cs
+++ USBSerialChecker/Program.cs
@@ -9,7 +9,7 @@ namespace USBSerialFinder
         static void Main(string[] args)
         {
             //Path where the txt file will be created. This is the path of the folder where the exe will be stored.
-            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string path = AppDomain.CurrentDomain.BaseDirectory + "FileName.txt";
             ManagementObjectSearcher Search = new ManagementObjectSearcher(Constants.Query);
             foreach (ManagementObject currentObject in Search.Get())
             {
